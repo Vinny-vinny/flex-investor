a4f3f025454c9570f4c4ddda8fd4d8238979ee40
vinnyvinn <kituyiv@gmail.com>
2026-01-29 10:32:14 +0300

    add end date

--- app/Http/Resources/ProductsResource.php
+++ app/Http/Resources/ProductsResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 class ProductsResource extends JsonResource
@@ -17,7 +18,8 @@ class ProductsResource extends JsonResource
             "id" => $this->id,
             "product_name" => $this->product_name." @".number_format($this->target_amount),
             "target_amount" => $this->target_amount,
-            "base_amount" =>  $this->base_amount
+            "base_amount" =>  $this->base_amount,
+            "end_date" => Carbon::now()->addYear()->format('d-m-Y'),
         ];
     }
 }
