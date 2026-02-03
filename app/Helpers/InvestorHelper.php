bbc550c9be51ff61c0c423354af3814ffe7df49e
vinnyvinn <kituyiv@gmail.com>
2026-02-03 09:11:33 +0300

    updates

--- app/Helpers/InvestorHelper.php
+++ app/Helpers/InvestorHelper.php
@@ -10,22 +10,22 @@ class InvestorHelper
 
     public static function genReferenceNumber(): string
     {
-        $prefix = 'INV52';
-        $ref = $prefix . strtoupper(
-                substr(
-                    str_shuffle('flexinvestornumbergenratrkenyabestfintechflexsackoaddedotthegame'),
-                    0,
-                    2
-                )
-                . Str::replaceFirst('O', 'K',
-                    Str::replaceFirst('i', 'L', Str::random(3))
-                )
-                . mt_rand(100, 999)
-            );
+        $prefix = 'INV52'; // 5 chars
+        $randomLength = 12 - strlen($prefix); // 7 chars
+
+        $random = strtoupper(
+            Str::replaceFirst(
+                'O', 'K',
+                Str::replaceFirst('I', 'L', Str::random($randomLength))
+            )
+        );
+
+        $ref = $prefix . $random;
 
         if (Invoice::where('invoice_number', $ref)->exists()) {
             return self::genReferenceNumber();
         }
+
         return $ref;
     }
 }
